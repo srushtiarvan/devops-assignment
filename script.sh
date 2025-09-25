9af76667754e119a6318d19cfdda67a0fe6b03fe
srushtiaravandekar <srushtia50@gmail.com>
2025-09-25 11:49:01 +0530

    Added assignment files

--- script.sh
+++ script.sh
@@ -3,17 +3,22 @@
 # Print working directory
 pwd
 
+#!/bin/bash
+
+# Print working directory
+pwd
+
 # Print current user
 whoami
 
-# Print memory available
-free -h
+# Print memory available (Windows)
+wmic MEMORYCHIP get Capacity
 
-# Print disk space
-df -h
+# Print disk space (Windows)
+df -h /c 2>/dev/null || df -h
 
 # List all files in current directory
 ls -la
 
-# Print contents of script.sh
+# Print contents of the script itself
 cat script.sh
